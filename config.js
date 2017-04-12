188e322426714382802ece67ab975d6608165973
Stefanie Hansen <stefuhnee@gmail.com>
2017-04-12 11:03:31 -0700

    replaced app info with placeholders

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // App's API information - replace with your own values
-  clientId: process.env.CLIENT_ID || '1878616a-02e3-4e3f-bfb3-9e91d214acfe',
-  clientSecret: process.env.CLIENT_SECRET || 'U4heHAHHhos3YqyWrSkC0A5',
+  clientId: process.env.CLIENT_ID || 'YOUR_ID_HERE',
+  clientSecret: process.env.CLIENT_SECRET || 'YOUR_SECRET_HERE',
   redirectUrl: process.env.REDIRECT_URI || 'http://localhost:3000/callback'
 };
